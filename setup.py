82eeb1ff31f11f929c6cfc6ab468365898e292a2
Louie S <louie@example.com>
2024-01-03 17:02:41 -0500

    Reverse orientation of OK and Cancel buttons

--- setup.py
+++ setup.py
@@ -2,4 +2,5 @@
 from setuptools import setup
 
 # For getting a git snapshot: ./setup.py egg_info -b "+git`date '+%Y%m%d'`" build sdist
+# For building on Windows: [/PATH/TO/]pyinstaller.exe --icon=data\assignment-list.ico -w --add-data=assignment_list_pyqt:assignment_list_pyqt assignment-list
 setup()
